f83436ffb80e2c133a2a470f4d73a6d460bcd88f
Nasim <alamnasim492@gmail.com>
2023-01-22 16:10:36 +0530

    My third project commit

---
+++ index.js
@@ -0,0 +1,44 @@
+function getData1() {
+    var req = new XMLHttpRequest()
+    req.open("GET", "https://restcountries.com/v3.1/name/india")
+    req.send()
+
+    req.addEventListener('load', function () {
+        var [data] = JSON.parse(this.responseText)
+        console.log(data);
+        document.getElementById('name').innerHTML = data.name.official
+        document.getElementById('capital').innerHTML = data.capital[0]
+        document.getElementById('borders').innerHTML = data.borders
+        document.getElementById('flags').src = data.flags.svg
+        document.getElementById('population').innerHTML = data.population
+        document.getElementById('region').innerHTML = data.region
+        document.getElementById('timezones').innerHTML = data.timezones
+        document.getElementById('subregion').innerHTML = data.subregion
+        document.getElementById('independent').innerHTML = data.independent
+       
+    })
+}
+getData1()
+
+function getData2() {
+    var cname = document.getElementById("cname").value
+    var req = new XMLHttpRequest()
+    req.open("GET", "https://restcountries.com/v3.1/name/" + cname)
+    req.send()
+
+    req.addEventListener('load', function () {
+        var [data] = JSON.parse(this.responseText)
+        console.log(data);
+        document.getElementById('name').innerHTML = data.name.official
+        document.getElementById('capital').innerHTML = data.capital[0]
+        document.getElementById('borders').innerHTML = data.borders
+        document.getElementById('flags').src = data.flags.svg
+        document.getElementById('population').innerHTML = data.population
+        document.getElementById('region').innerHTML = data.region
+        document.getElementById('timezones').innerHTML = data.timezones[0]
+        document.getElementById('subregion').innerHTML = data.subregion
+        document.getElementById('independent').innerHTML = data.independent
+      
+    })
+}
+getData2()
