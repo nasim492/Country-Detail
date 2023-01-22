207c96b0ac247ed6d62da0dec924ff28dfb097ea
Nasim <alamnasim492@gmail.com>
2023-01-22 16:27:45 +0530

    Resolve some issue in index.js

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ function getData1() {
         var [data] = JSON.parse(this.responseText)
         console.log(data);
         document.getElementById('name').innerHTML = data.name.official
-        document.getElementById('capital').innerHTML = data.capital[0]
+        document.getElementById('capital').innerHTML = data.capital
         document.getElementById('borders').innerHTML = data.borders
         document.getElementById('flags').src = data.flags.svg
         document.getElementById('population').innerHTML = data.population
         document.getElementById('region').innerHTML = data.region
         document.getElementById('timezones').innerHTML = data.timezones
         document.getElementById('subregion').innerHTML = data.subregion
-        document.getElementById('independent').innerHTML = data.independent
        
     })
 }
@@ -30,14 +29,13 @@ function getData2() {
         var [data] = JSON.parse(this.responseText)
         console.log(data);
         document.getElementById('name').innerHTML = data.name.official
-        document.getElementById('capital').innerHTML = data.capital[0]
+        document.getElementById('capital').innerHTML = data.capital
         document.getElementById('borders').innerHTML = data.borders
         document.getElementById('flags').src = data.flags.svg
         document.getElementById('population').innerHTML = data.population
         document.getElementById('region').innerHTML = data.region
         document.getElementById('timezones').innerHTML = data.timezones[0]
         document.getElementById('subregion').innerHTML = data.subregion
-        document.getElementById('independent').innerHTML = data.independent
       
     })
 }
